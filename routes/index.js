f8cb49df82a537103fb5f9f2c4f00f9946d86064
LEANDRO ROLDAN PERES <005839527@itaud.des.ihf>
2020-07-01 14:19:54 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('lexpress');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
